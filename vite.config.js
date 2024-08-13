e964c4977add7084e78bfafc63f89c0abed6bf95
Brandon Castro <66087694+bkcastro@users.noreply.github.com>
2024-08-13 09:55:42 +0100

    fixed npm run build issues

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,9 @@
 import { defineConfig } from 'vite';
 import wasm from 'vite-plugin-wasm';
-import fs from 'fs';
-import path from 'path';
 
 export default defineConfig({
     plugins: [wasm()],
     server: {
-        host: '0.0.0.0', // Bind to all network interfaces
-        https: {
-            key: fs.readFileSync(path.resolve(__dirname, 'key.pem')),
-            cert: fs.readFileSync(path.resolve(__dirname, 'cert.pem'))
-        }
+        port: 3000, // Bind to all network interfaces
     }
 });
